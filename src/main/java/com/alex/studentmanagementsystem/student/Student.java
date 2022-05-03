9bb7eff2b6d399e45eecdced4faefa31575a12d9
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2022-05-03 19:26:24 +0100

    Add student entity to db

--- src/main/java/com/alex/studentmanagementsystem/student/Student.java
+++ src/main/java/com/alex/studentmanagementsystem/student/Student.java
@@ -2,15 +2,22 @@ package com.alex.studentmanagementsystem.student;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 }
